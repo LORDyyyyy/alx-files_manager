624f6a07b39cebaa84e3154ae1f312cf4117b18e
mohamedtharwat000 <mohamed.sarwat.000@gmail.com>
2024-03-11 23:27:47 +0200

    Create UsersController.js

---
+++ controllers/UsersController.js
@@ -0,0 +1,22 @@
+import sha1 from 'sha1';
+import dbClient from '../utils/db';
+
+class UsersController {
+  static async postNew(req, res) {
+    const { email, password } = req.body;
+
+    if (!email) return res.status(400).send({ error: 'Missing email' });
+    if (!password) return res.status(400).send({ error: 'Missing password' });
+
+    const user = await dbClient.db.collection('users').findOne({ email });
+    if (user) return res.status(400).send({ error: 'Already exist' });
+
+    const result = await dbClient.db
+      .collection('users')
+      .insertOne({ email, password: sha1(password) });
+
+    return res.status(201).send({ id: result.insertedId, email });
+  }
+}
+
+module.exports = UsersController;
