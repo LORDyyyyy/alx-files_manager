7c2fe6b9ff06c60bbec83cd7ee740e53c7605ee4
mohamedtharwat000 <mohamed.sarwat.000@gmail.com>
2024-03-11 23:44:18 +0200

    Create FilesController.js

---
+++ controllers/FilesController.js
@@ -0,0 +1,29 @@
+import dbClient from '../utils/db';
+
+class UsersController {
+  static async postUpload(req, res) {
+    const { name, type, parentId } = req.body;
+    if (!name) {
+      return res.status(400).send({ error: 'Missing name' });
+    }
+    const parent = parentId
+      ? await dbClient.files.find({ _id: parentId })
+      : null;
+    if (parentId && !parent) {
+      return res.status(400).send({ error: 'Parent not found' });
+    }
+    const newFile = await dbClient.files.insertOne({
+      name,
+      type,
+      parentId,
+    });
+    return res.status(201).send({
+      id: newFile.insertedId,
+      name,
+      type,
+      parentId,
+    });
+  }
+}
+
+module.exports = UsersController;
