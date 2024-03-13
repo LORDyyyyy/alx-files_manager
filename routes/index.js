abf0ea6c86647a7920e869f17d1c64cb13824a97
mohamedtharwat000 <mohamed.sarwat.000@gmail.com>
2024-03-13 23:01:56 +0200

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -18,10 +18,18 @@ router.get('/users/me', AuthController.getMe);
 
 router.post('/files', MiddleWare.userAuth, FilesController.postUpload);
 
-router.get('/files', MiddleWare.userAuth, FilesController.getIndex);
 router.get('/files/:id', MiddleWare.userAuth, FilesController.getShow);
+router.get('/files', MiddleWare.userAuth, FilesController.getIndex);
 
-router.put('/files/:id/publish', MiddleWare.userAuth, FilesController.putPublish);
-router.put('/files/:id/unpublish', MiddleWare.userAuth, FilesController.putUnpublish);
+router.put(
+  '/files/:id/publish',
+  MiddleWare.userAuth,
+  FilesController.putPublish,
+);
+router.put(
+  '/files/:id/unpublish',
+  MiddleWare.userAuth,
+  FilesController.putUnpublish,
+);
 
 module.exports = router;
